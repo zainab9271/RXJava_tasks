e3c3d56ee25e9c23ba4708e29ab957aba28d4942
Bareq <bareqaltaamah@gmail.com>
2021-09-17 13:37:21 +0300

    init commit

---
+++ app/src/main/java/com/ibareq/rxjavatasks/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.ibareq.rxjavatasks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.ibareq.rxjavatasks.databinding.ActivityMainBinding
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    private val compositeDisposable = CompositeDisposable()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initObservers()
+    }
+
+    /**
+     * don't use long functions with multi responsibilities in your code
+     * we did this here for checking task result only
+     */
+    private fun initObservers() {
+        //TODO: uncomment any line to check its result
+
+//        RXJavaTasks.task1().observeOn(AndroidSchedulers.mainThread()).subscribe(::updateText).addTo(compositeDisposable)
+//        RXJavaTasks.task2().observeOn(AndroidSchedulers.mainThread()).subscribe(::updateText).addTo(compositeDisposable)
+//        RXJavaTasks.task3().observeOn(AndroidSchedulers.mainThread()).subscribe(::updateText).addTo(compositeDisposable)
+//        RXJavaTasks.task4().observeOn(AndroidSchedulers.mainThread()).subscribe(::updateText).addTo(compositeDisposable)
+//        RXJavaTasks.task5().observeOn(AndroidSchedulers.mainThread()).subscribe(::updateText).addTo(compositeDisposable)
+//        RXJavaTasks.task6().observeOn(AndroidSchedulers.mainThread()).subscribe(::updateText).addTo(compositeDisposable)
+//        RXJavaTasks.task7().observeOn(AndroidSchedulers.mainThread()).subscribe(::updateText).addTo(compositeDisposable)
+
+    }
+
+    private fun updateText(newValue: Any){
+        binding.labelRxData.text = newValue.toString()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
+    }
+}
---
+++ app/src/main/java/com/ibareq/rxjavatasks/RXJavaTasks.kt
@@ -0,0 +1,69 @@
+package com.ibareq.rxjavatasks
+
+import io.reactivex.rxjava3.core.Observable
+import java.util.concurrent.TimeUnit
+
+/**
+ * Don't update function name or return type
+ * only add function body
+ */
+
+object RXJavaTasks {
+
+
+    /**
+     * Complete below function
+     * let it emit characters form A to Z each 1 second
+     */
+    fun task1(): Observable<String> {
+
+    }
+
+    /**
+     * Complete below function
+     * let it emit characters form A to Z each 1 second
+     */
+    fun task2(): Observable<String> {
+
+    }
+
+    /**
+     * Complete below function
+     * let it emit characters form A to Z each 1 second
+     */
+    fun task3(): Observable<String> {
+
+    }
+
+    /**
+     * Complete below function
+     * let it emit characters form A to Z each 1 second
+     */
+    fun task4(): Observable<String> {
+
+    }
+
+    /**
+     * Complete below function
+     * let it emit characters form A to Z each 1 second
+     */
+    fun task5(): Observable<String> {
+
+    }
+
+    /**
+     * Complete below function
+     * let it emit characters form A to Z each 1 second
+     */
+    fun task6(): Observable<String> {
+
+    }
+
+    /**
+     * Complete below function
+     * let it emit characters form A to Z each 1 second
+     */
+    fun task7(): Observable<String> {
+
+    }
+}
